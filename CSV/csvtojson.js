28bae926ae3606205206bef6ddfcc8d5f638faab
NauoraM <46752099+NauoraM@users.noreply.github.com>
2019-09-13 13:54:32 +0200

    Update csvtojson.js

--- CSV/csvtojson.js
+++ CSV/csvtojson.js
@@ -1,6 +1,6 @@
-let csvToJson = require('convert-csv-to-json');
+let csvToJson = require('convert-csv-to-json'); //Den här hämtar modulen.
  
-let fileInputName = 'labb1.csv';
+let fileInputName = 'labb1.csv'; //Här hittar den filerna och de ligger i olika variablar.
 let fileOutputName = 'myOutputFile.json';
  
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName); //Här skickar den info till en annan fil som kommer att omvandla den till en json fil.
